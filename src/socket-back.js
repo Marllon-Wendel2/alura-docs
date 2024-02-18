933f48c316592f5c02c11931537cc84a761dd59e
Marllon Wendel <m.w.p.ferreira@outlook.com>
2024-02-18 15:17:00 -0300

    Relacionando link com o banco

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,13 @@
-import { encontrarDocumento, atualizaDocumento } from "./documentosDb.js";
+import { encontrarDocumento, atualizaDocumento, obterDocumentos } from "./documentosDb.js";
 import io from "./servidor.js";
 
 
 io.on("connection", (socket) => {
-  console.log("Um cliente se conectou! ID:", socket.id);
+  socket.on("obter_documentos", async (devolverDocumentos) => {
+    const documentos = await obterDocumentos();
+
+    devolverDocumentos(documentos)
+  });
   
   socket.on("selecionar_documento", async (nomeDocumento, devolverTexto) => {
     socket.join(nomeDocumento);
